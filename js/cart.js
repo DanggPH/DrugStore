39244e421966151293ae989f78a137cdbe264658
baovipboy156 <baovipboy156@gmail.com>
2021-04-15 18:36:29 +0700

    wtf
    wtf

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,9 @@
 function updateCart(){
 	document.getElementsByClassName('cart_no')[0].innerHTML=document.getElementsByClassName('cart-item').length
 }
+function updatePrice(){
+	document.getElementsByClassName('cart-item').forEach(myFunction)
+}
+function myFunction(item, index, arr) {
+  console.log(item)
+}
