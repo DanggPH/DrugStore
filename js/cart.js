a705e010016c08bab1e6e6822b82b6d88b52a42d
baovipboy156 <baovipboy156@gmail.com>
2021-04-18 22:19:14 +0700

    tính tổng giá

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,11 @@ function updateCart(){
 	document.getElementsByClassName('cart_no')[0].innerHTML=document.getElementsByClassName('cart-item').length
 }
 function updatePrice(){
-	document.getElementsByClassName('cart-item').forEach(myFunction)
+	var total = 0;
+	for (var i = 0; i < document.getElementsByClassName('cart-item').length; i++) {
+	 	total += parseInt(document.getElementsByClassName('cart-item')[i].children[2].children[0].innerHTML);
+	}
+	document.getElementById("total-price").innerHTML=total
 }
 function myFunction(item, index, arr) {
   console.log(item)
