99ffa19d86782b0d59f58ed6d3f42934cc2a6132
baovipboy156 <baovipboy156@gmail.com>
2021-04-19 19:16:23 +0700

    xong cart

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,7 @@
 function updateCart(){
 	document.getElementsByClassName('cart_no')[0].innerHTML=document.getElementsByClassName('cart-item').length
+	var temp = document.getElementById("marker");
+	document.getElementById("cart-list").appendChild(temp);
 }
 function updatePrice(){
 	var total = 0;
@@ -8,6 +10,54 @@ function updatePrice(){
 	}
 	document.getElementById("total-price").innerHTML=total
 }
-function myFunction(item, index, arr) {
-  console.log(item)
+function addToCart(item){
+	var liClass = document.createElement("li");
+	var DivContainer = document.createElement("div");
+
+	DivContainer.setAttribute("class","cart-item");
+
+	var imageContainer = document.createElement("div");
+	imageContainer.setAttribute("class","image");
+	var image = document.createElement("img");
+	image.setAttribute("src",item.parentElement.parentElement.children[0].children[0].children[0].getAttribute("src"));
+
+	var descripsion = document.createElement("div");
+	descripsion.setAttribute("class","item-description");
+
+	var dis = document.createElement("p");
+	dis.setAttribute("class","name");
+	dis.innerHTML="sản phẩm clgt";	
+	var dos = document.createElement("p");
+	dos.innerHTML="Quantity: ";
+	var dus = document.createElement("span");
+	dus.setAttribute("class","light-red");
+	dus.innerHTML="01";
+	dos.appendChild(dus);
+
+	descripsion.appendChild(dis)
+	descripsion.appendChild(dos);
+
+
+	var right = document.createElement("div");
+	right.setAttribute("class","right");
+	var das = document.createElement("p");
+	das.setAttribute("class","price");
+	das.innerHTML=item.parentElement.parentElement.children[2].innerHTML;
+	var ref = document.createElement("a");
+	ref.setAttribute("href","#");
+	ref.setAttribute("class","remove");
+	ref.setAttribute("onclick","this.parentElement.parentElement.parentElement.remove();");
+	var aimg = document.createElement("img");
+	aimg.setAttribute("src","images/remove.png");
+	aimg.setAttribute("alt","remove");
+	ref.appendChild(aimg);
+	right.appendChild(das);
+	right.appendChild(ref);
+
+	imageContainer.appendChild(image);
+	DivContainer.appendChild(imageContainer);
+	DivContainer.appendChild(descripsion);
+	DivContainer.appendChild(right);
+	liClass.appendChild(DivContainer);
+	document.getElementById("cart-list").appendChild(liClass);
 }
